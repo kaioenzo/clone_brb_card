f2f230bf539571890d5e8cc826ca00c3e9470d26
Kaio Enzo <kaioenzobr@gmail.com>
2025-02-10 12:11:49 -0300

    adiciona tela inicial e atualiza o título do aplicativo

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:clone_brb_card/ui/home/widgets/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,11 +11,8 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      title: 'BRB CARD',
+      home: HomeScreen(),
     );
   }
 }
---
+++ lib/ui/home/widgets/home_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [Color(0xFF3C6AB2), Color(0xFFFFFFFF)],
+          stops: [0.0, 0.4512],
+        ),
+      ),
+    );
+  }
+}
