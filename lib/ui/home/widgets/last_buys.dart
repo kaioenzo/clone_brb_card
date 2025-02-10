847e91b1805e1dec1d2615e0e584591147005c45
Kaio Enzo <kaioenzobr@gmail.com>
2025-02-10 15:16:39 -0300

    corrige importação do modelo de transação para o nome correto

--- lib/ui/home/widgets/last_buys.dart
+++ lib/ui/home/widgets/last_buys.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:intl/intl.dart';
 
-import '../../../domain/models/transacation.dart';
+import '../../../domain/models/transaction.dart';
 import '../../../widgets/divider.dart';
 import '../view_models/home_viewmodel.dart';
 
